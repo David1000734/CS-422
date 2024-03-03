af3713da84ae07e48e2c7740c02be295668fe838
David <lam.david459@yahoo.com>
2024-03-02 19:17:14 -0800

    Start assignment

--- Homework3/regression.py
+++ Homework3/regression.py
@@ -2,4 +2,17 @@ import pandas as pd
 import numpy as np
 import math
 
+# ******************** REMOVE ********************
+# https://mkang.faculty.unlv.edu/teaching/CS489_689/code3/Linear_Regression.html
+# https://www.youtube.com/watch?v=VmbA0pi2cRQ
+# ******************** REMOVE ********************
+
+
+
+data = pd.read_csv("auto-mpg.data.csv")
+
+# Drop the last row (Car Name)
+data = data.iloc[:, :-1]
+print(data)
+
 
