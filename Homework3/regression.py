d927341209daea2c0f2c54ee545b261a0c4903c8
David <lam.david459@yahoo.com>
2024-03-09 20:13:48 -0800

    Added linear regression function
    Will be normalizing data set to do regression and then unnormalizing later
    A bit ugly but should work...

--- Homework3/regression.py
+++ Homework3/regression.py
@@ -8,6 +8,7 @@ import math
 # https://www.youtube.com/watch?v=VmbA0pi2cRQ
 # https://www.youtube.com/watch?v=P8hT5nDai6A
 # https://www.youtube.com/watch?v=ltXSoduiVwY
+# https://www.youtube.com/watch?v=sRh6w-tdtp0
 # Multiple linear Regression
 # x1, x2, x3, xn
 # m1, m2, m3, mn
@@ -40,6 +41,52 @@ def normalize_Data(data_Column):
     return new_Data_List
 # Normalize_Data funct, END
 
+def un_normalize_Data(data_Column, max, min):
+    """
+    Given that we know what the specific data's min and max is, 
+    we can get it's original value by the following formula:
+    X: is norm-val
+    Un-normalized val = X * (max - min) + min
+
+    :param data_Column: Normalized data
+    :param max: Original Max value of data
+    :param min: Original Min value of data
+    :return: un-normalized data row
+    """
+    unNorm_Data_List = []
+
+    for i in range(len(data_Column)):
+        print(data_Column[i])
+
+    return unNorm_Data_List
+
+# *************** CHANGE THE CODE DOWN HERE ***************
+def linear_Model(x_val, y_target, learning_rate, iteration, arr):
+    m = y_target.size
+    # Create column of 0's of the same size as x's features
+    theta = np.zeros((x_val.shape[1], 1))
+
+    for i in range(iteration):
+        y_pred = np.dot(x_val, theta)
+
+        # Cost Function
+        # 1/2m Sum(y_pred - Y)^2, where Y is the actual value
+        cost = ((1 / (2 * m)) * np.sum((y_pred - y_target) ** 2, axis = 0))
+        # cost = (1/(2*m))*np.sum(np.square(y_pred - y_target))
+
+        # Gradient Descent
+        # d_theta = 1/m(matrix_mul(X^T, y_pred - Y))
+        d_theta = ((1 / m) * np.dot(x_val.transpose(), y_pred - y_target))
+        # d_theta = (1/m)*np.dot(x_val.T, y_pred - y_target)
+
+        # theta = theta - alpha * d_theta
+        theta = theta - learning_rate * d_theta
+        # print(theta)
+
+        arr.append(cost)
+
+    return theta
+
 # To be removed...
 def mean_Error(m, b, points):
     total_error = 0
@@ -51,6 +98,9 @@ def mean_Error(m, b, points):
 # To be removed...
 
 # ******************** Main ********************
+
+lr_lambda = 1               # Learning Rage
+max_Iteration = 10000       # Iterations
 
 # Drop the last row (Car Name)
 data = data.iloc[:, :-1]
@@ -68,5 +118,25 @@ for i in range(len(data.columns)):
     build_DF.insert(i, columnName, temp_DF, True)
 # For, END
 
-print(np.corrcoef(build_DF))
-print(build_DF)
+# Normalize all data and then un-normalize later
+# X is all values besides MPG
+x = build_DF.iloc[ : , 1 : 7]
+
+# Y is the MPG, the predicting value
+y = build_DF.iloc[ : , 0 : 1]
+
+# Adding a column of 1's
+# x["theta_0"] = 1
+cost_arr = []
+
+theta = linear_Model(x, y, lr_lambda, max_Iteration, cost_arr)
+un_normalize_Data(cost_arr, 32, 18)
+#print(cost_arr)
+
+rng = np.arange(0, max_Iteration)
+plt.plot(rng, cost_arr)
+plt.show()
+
+
+# print(np.corrcoef(build_DF))
+# print(build_DF)
