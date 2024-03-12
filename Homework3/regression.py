a932188089a5b04cf98dcbaa55d243c6f94520f6
David <lam.david459@yahoo.com>
2024-03-11 21:02:41 -0700

    Added k-fold function
    Next task is to add the logic to separate rows of data

--- Homework3/regression.py
+++ Homework3/regression.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import random as rand
+import math
 
 # ******************** REMOVE ********************
 # https://mkang.faculty.unlv.edu/teaching/CS489_689/code3/Linear_Regression.html
@@ -62,7 +64,7 @@ def un_normalize_Data(data, max, min):
     :param data: Normalized data point
     :param max: Original Max value of data
     :param min: Original Min value of data
-    :return: un-normalized data row
+    :return: un-normalized data point
     """
 
     return data * (max - min) + min
@@ -87,13 +89,99 @@ def linear_Model(x_val, y_target, learning_rate, iteration, arr):
         # d_theta = (1/m)*np.dot(x_val.T, y_pred - y_target)
 
         # theta = theta - alpha * d_theta
-        theta = theta - learning_rate * d_theta
+        theta -= learning_rate * d_theta
         # print(theta)
 
         # Un-Norm data and store into array
         arr.append(un_normalize_Data(cost, mpg_Max, mpg_Min))
 
-    return theta
+    # Un-Norm data
+    for j in range(len(y_pred)):
+        # Un-normalize and also round to nearest int
+        y_pred[j] = np.round(un_normalize_Data(y_pred[j], mpg_Max, mpg_Min))
+        # y_pred[j] = un_normalize_Data(y_pred[j], mpg_Max, mpg_Min)
+
+    return y_pred
+# *************** CHANGE THE CODE UP HERE ***************
+
+def k_fold_Test(arr, folds):
+    isDivisible = True
+    train_arr = pd.DataFrame()        # Val to train the prediction
+    test_arr = pd.DataFrame()         # Predicted values from training
+    multiplier = 0                    # Used to find what rows to get
+
+    if (folds < 2 or folds > 100):
+        print("Error: K-Folds must be higher than 2 and less than 100.")
+        return
+    # If, END
+
+    # To Ensure we are not geting from test, we must
+    # do the getting inside the for loop
+    if (len(arr) % folds != 0):
+        isDivisible = False
+        multiplier = len(arr) / folds
+    else :
+        multiplier = len(arr) / folds
+        multiplier = math.ceil(multiplier)
+    # if else, END
+    multiplier = int(multiplier)
+
+    # Loop number of folds
+    for fold_I in range(folds):
+        temp_I = fold_I             # Used for k-fold test
+        # Separate data
+        # Get rows of data via fold_I and multiplier. use .iloc
+
+
+
+        # Find if the folds is divisible by the array.
+        # If it is, great, if not, fill in missing by randomly
+        # getting columns from the array. Ensure not to grab from test
+        if (not isDivisible):
+            # Since mod is not 0, fill in missing rows
+            rem = len(arr) % folds
+
+            # Iterate however many times we need to fill in rows
+            for rand_I in range(rem, (folds - 1)):
+                while (temp_I == fold_I):
+                    # Rand range between 0 and 10, NOT including 10
+                    temp_I = rand.randrange(0, 10)
+                    # Keep doing random numbers until one not inside
+                    # the test set is picked.
+                # While, END
+
+                # Find random val within range
+                randRow = rand.randrange(temp_I * multiplier,
+                                         (temp_I + 1) * multiplier)
+                # Get the row
+                dup_Row = arr.iloc[randRow , : ]
+                print(dup_Row)                
+                test_arr = pd.concat([test_arr, dup_Row], ignore_index = True)
+                test_arr.reset_index()
+            # For, END
+        # If notDivisible, END
+        print(test_arr)
+
+        # Loop the entire test set
+        for test_I in range(len(train_arr)):
+            
+            
+            
+            pass
+
+
+        # Check predicted value with test array
+        for pred_I in range(len(test_arr)):          # Only check however many is in my test array
+
+            pass
+
+        # Clear both arrays
+        test_arr = test_arr.iloc[0 : 0]
+        train_arr = train_arr.iloc[0 : 0]
+    pass
+    # For k-folds, END
+
+# Funct K_Fold_Test, END 
 
 # To be removed...
 def mean_Error(m, b, points):
@@ -134,13 +222,31 @@ x = build_DF.iloc[ : , 1 : 7]
 y = build_DF.iloc[ : , 0 : 1]
 
 # Adding a column of 1's
-# x["theta_0"] = 1
+x["theta_0"] = 1
 cost_arr = []
 
-theta = linear_Model(x, y, lr_lambda, max_Iteration, cost_arr)
-print(cost_arr)
+k_fold_Test(build_DF, 10)
 
-rng = np.arange(0, max_Iteration)
-plt.plot(rng, cost_arr)
-plt.show()
+
+
+
+
+
+# predict = linear_Model(x, y, lr_lambda, max_Iteration, cost_arr)
+# print(predict)
+# # print(y)
+# # print(cost_arr)
+# # print(cv)
+
+# if (predict[-1] == 30):
+#     print("It's fine")
+# else:
+#     print("Is not find")
+
+# # for i in range (len(theta)):
+# #     print(un_normalize_Data(theta[i], mpg_Max, mpg_Min))
+
+# rng = np.arange(0, max_Iteration)
+# plt.plot(rng, cost_arr)
+# plt.show()
 
