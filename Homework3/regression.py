788e196a248a020eb6d2597eeba413d6d09201c4
David <lam.david459@yahoo.com>
2024-03-15 16:37:01 -0700

    Clean-up
    Cleaned up a lot of functions
    Changed data so that it would read from auto-mpg.data instead of csv.
    Accommodating functions and logic for the switch was changed as well.

--- Homework3/regression.py
+++ Homework3/regression.py
@@ -1,29 +1,17 @@
 import pandas as pd
+pd.options.mode.chained_assignment = None       # Silence warning
 import numpy as np
 import matplotlib.pyplot as plt
 import random as rand   # Random number
 import math             # Using round function
-import statistics       # Using median function
-from sklearn.linear_model import LinearRegression
-# ******************** REMOVE ********************
-# https://mkang.faculty.unlv.edu/teaching/CS489_689/code3/Linear_Regression.html
-# https://www.youtube.com/watch?v=VmbA0pi2cRQ
-# https://www.youtube.com/watch?v=P8hT5nDai6A
-# https://www.youtube.com/watch?v=ltXSoduiVwY
-# https://www.youtube.com/watch?v=sRh6w-tdtp0
-# Multiple linear Regression
-# x1, x2, x3, xn
-# m1, m2, m3, mn
-# Y = m1x1 + m2x2 + m3x3 + mnxn + c
-# c -> y-intercept
-# ******************** REMOVE ********************
 
-# field_name_data = ["mpg", "cylinders", "displacement",
-#           "horsepower", "weight", "acceleration",
-#           "model_year", "origin", "carname"]
-data = pd.read_csv("auto-mpg.data.csv")
+name_data = ["mpg", "cylinders", "displacement",
+          "horsepower", "weight", "acceleration",
+          "model_year", "origin", "carname"]
+#data = pd.read_csv("auto-mpg.data.csv")
 
-#data = pd.read_fwf("auto-mpg.data", names = field_name)
+data = pd.read_fwf("auto-mpg.data", names = name_data)
+# Only 6 entrys in horsepower have ?
 
 # Storing the global min and max of the mpg for later
 mpg_Max = 0
@@ -36,7 +24,9 @@ ten_fold_table = pd.DataFrame()
 def normalize_Data(data_Column):
     """
     normalize_Data will utilize Min-Max Scaling to set
-    values between 0 - 1.
+    values between 0 - 1. Will also take into account
+    potential missing values. If found will replace with
+    it's median. Additionally, column is change to a float.
     
     :param data_Column: Recieve the column to be normalized
     :return: Output the normalized column in dataType List
@@ -51,6 +41,18 @@ def normalize_Data(data_Column):
         mpg_Max = max(data_Column)
         mpg_Min = min(data_Column)
 
+    # Check to see if there are missing data
+    if (not data_Column[data_Column == '?'].empty):
+        # Change all ? into None to allow of processing
+        data_Column[np.where('?' == data_Column)] = None
+        # Change into float
+        data_Column = data_Column.astype(float)
+        # Find median
+        mid = round(data_Column.median(skipna = True))
+        # Search for NaN and replace with median
+        data_Column[data_Column.index[data_Column.apply(np.isnan)]] = mid
+        data.horsepower = data_Column
+    
     # Bottom value will remain constent throughout loop
     bottom = max(data_Column) - min(data_Column)        # x_Max - x_Min
 
@@ -81,7 +83,17 @@ def un_normalize_Data(data, max = mpg_Max, min = mpg_Min):
 
 # *************** CHANGE THE CODE DOWN HERE ***************
 def linear_Model(x_val, y_target, learning_rate, iteration):
-    cost_arr = []
+    """
+    Functin will provide the coefficients after performing a gradient descent
+    to find the value that most closely matches the given x and y
+
+    :param x_val: Array of features used to predict y
+    :param y_target: Array of the target value.
+    :param learning_rate: Lambda or how fast/slow to step in gradient descent
+    :param iteration: How many iteration should it have
+
+    :return: Output a coefficient series of x feature size.
+    """
     m = y_target.size
     # Create column of 0's of the same size as x's features
     theta = np.zeros((x_val.shape[1], 1))
@@ -92,33 +104,35 @@ def linear_Model(x_val, y_target, learning_rate, iteration):
         # Cost Function
         # 1/2m Sum(y_pred - Y)^2, where Y is the actual value
         cost = ((1 / (2 * m)) * np.sum((y_pred - y_target) ** 2, axis = 0))
-        # cost = (1/(2*m))*np.sum(np.square(y_pred - y_target))
 
         # Gradient Descent
         # d_theta = 1/m(matrix_mul(X^T, y_pred - Y))
         d_theta = ((1 / m) * np.dot(x_val.transpose(), y_pred - y_target))
-        # d_theta = (1/m)*np.dot(x_val.T, y_pred - y_target)
 
         # theta = theta - alpha * d_theta
         theta -= learning_rate * d_theta
-        # cost_arr.append(un_normalize_Data(cost, mpg_Max, mpg_Min))        # DEBUG
-        # if (i % 1000 == 0):
-        #     print(un_normalize_Data(cost, mpg_Max, mpg_Min))     # DEBUG)
+        if (i % 1000 == 0):
+            print(un_normalize_Data(cost, mpg_Max, mpg_Min))
 
-
-    # DEBUG
-    # rng = np.arange(0, iteration)
-    # plt.plot(rng, cost_arr)
-    # plt.show()
-    # DEBUG
     return theta
 # *************** CHANGE THE CODE UP HERE ***************
 
-
-# CURRENTLY, Function likely can only return 1 row at max.
-# It should be able to return up to how every many rows are
-# needed for the array
 def get_DupRows(source_arr, folds, multip, cur_Iter):
+    """
+    Idea was for the function to duplicate ANY NUMBER of rows
+    as needed to balance the train array. However, got lazy,
+    function can only return a single row.
+
+    :param source_arr: Main array to read from
+    :param folds: Max fold count
+    :param multip: Multiplier, block, or size of each fold.
+    :param cur_Iter: Current iteration the main loop is currently at.
+
+    :return: Output the duplicated row
+
+    :note: Fixing this function would likely fix the 
+    k-fold function not being able to handle odd value folds
+    """
     temp_I = 0
     # Since mod is not 0, fill in missing rows
     rem = len(source_arr) % folds
@@ -141,6 +155,17 @@ def get_DupRows(source_arr, folds, multip, cur_Iter):
     return source_arr.iloc[randRow : randRow + 1 , : ]
 
 def pred_values(coefficients, actual_val_Row):
+    """
+    Given a set of coefficients, loop through all of them
+    and attempt to predict a value. 
+    Equation is: x1 * v1 + x2 * v2 + ... + xFin * vFin
+    Where x is the coefficients and v is the parameters
+
+    :param coefficients: Coefficient given from linear regression
+    :param actual_val_Row: Row of the actual value
+
+    :return: Predicted value
+    """
     predicted_Value = 0
     mul = 0
 
@@ -151,14 +176,30 @@ def pred_values(coefficients, actual_val_Row):
     return predicted_Value
 
 def k_fold_Test(arr, folds):
+    """
+    Function will attempt to perform the k-fold testing with
+    a specified number of folds. Sufficient for 10 or even folds. 
+    Function don't do so well with odd folds and too scared to test
+    with larger number of folds...
+
+    :param arr: Complete array for folds to perform on
+    :param folds: Number of folds it should do
+
+    :return: best performing fold number, coefficients, and percentage.
+
+    :note: Relies on various functions and a global initilized 
+           "ten_fold_table" to store to. pred_values, get_DupRows,
+           linear_Model and un_normalize_Data function
+    """
     global ten_fold_table             # Build final table
     isDivisible = True
     train_arr = pd.DataFrame()        # Val to train the prediction
     test_arr = pd.DataFrame()         # Predicted values from training
     multip = 0                        # Used to find what rows to get
     max_coef = pd.DataFrame()         # Store best coefficients
+    max_fold = 0                      # Store best fold
     margin_error = 3                  # If value lands within #, count as valid
-    coef_DR = pd.DataFrame()          # Building table
+    highest_accur = 0
 
     if (folds < 2 or folds > 100):
         print("Error: K-Folds must be higher than 2 and less than 100.")
@@ -185,10 +226,11 @@ def k_fold_Test(arr, folds):
         # Separate data
         # Get rows of data via fold_I and multiplier. use .iloc
         if (fold_I == 0):
+            # First block is testing, all else is train
             test_arr = arr.iloc[start_Block : end_Block, :]
             train_arr = arr.iloc[end_Block : , :]
         else: 
-            # For all other I values, the test data is inbetween train data
+            # For all other i values, the test data is inbetween train data
             test_arr = arr.iloc[start_Block : end_Block, :]
 
             # Get first set of data
@@ -207,8 +249,8 @@ def k_fold_Test(arr, folds):
             test_arr = pd.concat([test_arr, temp_Row], ignore_index = True)
             test_arr.reset_index()
 
-                # CURRENTLY, Does not account for potentially grabbing
-                # from the test set, Light Cheating, Should fix...
+            # CURRENTLY, Does not account for potentially grabbing
+            # from the test set, Light Cheating, Should fix...
 
             # Do the same for train_arr
             temp_Row = get_DupRows(arr, folds, multip, fold_I)
@@ -216,20 +258,18 @@ def k_fold_Test(arr, folds):
             train_arr.reset_index()
         # If notDivisible, END
 
-        # Data processing is complete, now do the testing
-
+        # *************** Data Processing complete, start testing ***************
         # For Train Data
         # Get X and Y
         train_X = train_arr.iloc[ :, 1 :]
         train_Y = train_arr.iloc[ :, : 1]
         found_val = 0           # Predicted Value
         act_val = 0             # Actual Value
-        highest_accur = 0
         correct_count = 0
         rmse = 0                # Root Mean Square Error
         coef_series = []
 
-        coef = linear_Model(train_X, train_Y, 0.0005, 10000)
+        coef = linear_Model(train_X, train_Y, lr_lambda, max_Iteration)
         # Check predicted value with test array
         for i in range(len(test_arr)):          # Only check however many is in my test array
             # Only give it the features from the test array
@@ -239,28 +279,29 @@ def k_fold_Test(arr, folds):
 
             # Compare with actual value
             act_val = un_normalize_Data(test_arr.iloc[i, 0], mpg_Max, mpg_Min)
-            print(found_val)        # DEBUG
-            print(act_val)          # DEBUG
+
             if (found_val > act_val - margin_error and
                 found_val < act_val + margin_error):
                 correct_count += 1
 
             if ((correct_count / len(test_arr)) > highest_accur):
                 max_coef = coef
+                max_fold = (fold_I + 1)
                 highest_accur = correct_count / len(test_arr)
+                print(highest_accur)
+                print(max_fold)
         # For, END
         # Root Mean Square Error
         # Sqrt(sum from 1 to N(actual - predict)^2))
         rmse = sum((act_val - found_val) ** 2)
         rmse = round(math.sqrt(rmse), 2)
-        coef = coef.reshape(1, 6)
-        print(coef)
+        coef = coef.reshape(1, train_X.shape[1])
 
         # Un-standardize all data
         for i in range(len(data.columns)):
             # Skip mpg as coef does not have it
             if (i == 0):
-                continue
+                continue    # Cheap fix
             # To un-norm, we have to know the min and max of
             # each respective column.
             un_norm_data = round(un_normalize_Data(coef[0][i - 1],
@@ -268,41 +309,40 @@ def k_fold_Test(arr, folds):
                                           min(data.iloc[ :, i])))
             coef_series.append(un_norm_data)
         # For, END
-        
-        # There is no "origin" in the .csv file so none will be added
-        coef_series.append(None)
 
+        # *************** Testing complete, build table ***************
         # add the Root mean Squared Error
         coef_series.append(rmse)
         
-        coef_series = np.reshape(coef_series, (1, 8))
-        #coef_DR = pd.DataFrame(coef_series, columns = field_name)
+        coef_series = np.reshape(coef_series, (1, (train_X.shape[1] + 1)))
         ten_fold_table = pd.concat([ten_fold_table,
-                                   pd.DataFrame(coef_series, columns = field_name)],
+                                   pd.DataFrame(coef_series, columns = table_name)],
                                    ignore_index = True)
         ten_fold_table = ten_fold_table.rename(index = temp_dict)
 
-        print(coef)
-        print(rmse)
+        # Leaving print to see program is running
         print(ten_fold_table)
+        print("Correct Guesses: %i \nPercent Correct: %i%%" %
+              (correct_count, round((correct_count / len(test_arr) * 100), 2)))
         # Clear both arrays
         test_arr = test_arr.iloc[0 : 0]
         train_arr = train_arr.iloc[0 : 0]
     # For k-folds, END
 
-    return max_coef, round((highest_accur * 100), 2)
+    return max_fold, max_coef, round((highest_accur * 100), 2)
 # Funct K_Fold_Test, END 
 
 # ******************** Main ********************
 
 # names, WITHOUT mpg and car name
-field_name = ["cylinders", "displacement",
+table_name = ["cylinders", "displacement",
           "horsepower", "weight", "acceleration",
           "model_year", "origin", "RMSE"]
-lr_lambda = 0.5               # Learning Rate
+lr_lambda = 0.0005            # Learning Rate
 max_Iteration = 10000         # Iterations
 k_folds = 10                  # Define fold numbers
 
+# Create the row names
 temp_dict = {}
 # Set up row names
 for i in range(k_folds):
@@ -331,32 +371,9 @@ x = build_DF.iloc[ : , 1 : ]
 # Y is the MPG, the predicting value
 y = build_DF.iloc[ : , 0 : 1]
 
-# Adding a column of 1's
-#x["theta_0"] = 1
+# Function returns two variables
+bestFold, highestCoef, highestPerc = k_fold_Test(build_DF, k_folds)
 
-#reg = LinearRegression().fit(x, y)
-
-# print(reg.score(x,y))
-# print(reg.coef_)
-# print(sum(reg.coef_))
-# print(reg.intercept_)
-
-# print(reg.predict(np.array([[0.5, 0.6, 0.4, 0.9, 1, 0.5, 0.1]])))
-
-print(k_fold_Test(build_DF, k_folds))
-
-
-print(ten_fold_table)
-# predict = linear_Model(x, y, lr_lambda, max_Iteration)
-# print(predict)
-# # print(y)
-# # print(cost_arr)
-# # print(cv)
-
-# if (predict[-1] == 30):
-#     print("It's fine")
-# else:
-#     print("Is not find")
-
-# # for i in range (len(theta)):
-# #     print(un_normalize_Data(theta[i], mpg_Max, mpg_Min))
+print("Highest percent is %i%% from fold %i." % (highestPerc, bestFold))
+# Final result here, although function prints it already
+# print(ten_fold_table)
